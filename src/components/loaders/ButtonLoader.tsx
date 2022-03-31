fdfdd045b86dc121f3575938b520f274a85e6092
Grasiela Gomes <produtoradama@gmail.com>
2022-03-31 18:05:07 -0300

    fix: change button loader color by attribute

--- src/components/loaders/ButtonLoader.tsx
+++ src/components/loaders/ButtonLoader.tsx
@@ -1,5 +1,11 @@
 import BeatLoader from 'react-spinners/BeatLoader'
 
-export const ButtonLoader = () => <BeatLoader color="white" size={8} />
+interface IButtonLoader {
+  isOutlined?: boolean
+}
+
+export const ButtonLoader = ({ isOutlined }: IButtonLoader) => (
+  <BeatLoader color={isOutlined ? 'gray' : 'white'} size={8} />
+)
 
 export default ButtonLoader
