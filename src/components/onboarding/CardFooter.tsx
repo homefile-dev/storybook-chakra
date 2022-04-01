eabe22677569e7fabb8c60dfec9ffd00f3caf05c
Grasiela Gomes <produtoradama@gmail.com>
2022-04-01 19:12:03 -0300

    fix: change CardFooter style

--- src/components/onboarding/CardFooter.tsx
+++ src/components/onboarding/CardFooter.tsx
@@ -2,20 +2,23 @@ import { Box, Container, Text, Button } from '@chakra-ui/react'
 import { ICardFooter } from '../../interfaces/onboarding/CardFooter.interface'
 
 export const CardFooter = ({
-  title,
+  title = '',
   buttonLabel,
   handleButtonClick,
 }: ICardFooter) => {
   return (
-    <Container variant="secondary" centerContent h="10rem">
+    <Container variant="secondary" centerContent>
       <Box
         w="full"
         px={['container.sm', 'container.md', 'container.lg']}
         my="auto"
+        py="2rem"
       >
-        <Text mb={4} textAlign="center" variant="body">
-          {title}
-        </Text>
+        {title !== '' && (
+          <Text mb={4} textAlign="center" variant="body">
+            {title}
+          </Text>
+        )}
         <Button mb={2} variant="secondary" onClick={handleButtonClick}>
           {buttonLabel}
         </Button>
