19a7638beaf1217538391f74a9019bbc6ba16fb1
Grasiela Gomes <produtoradama@gmail.com>
2022-04-04 20:43:05 -0300

    feat: add MyHomes component

---
+++ src/components/myHomes/MyHomes.tsx
@@ -0,0 +1,22 @@
+import { Container } from '@chakra-ui/react'
+import { t } from 'i18next'
+import { FiMoreHorizontal } from 'react-icons/fi'
+import { CustomIcon } from '../icons/CustomIcon'
+import ContainerHeader from '../launchpad/ContainerHeader'
+import House from '../../assets/images/house.svg'
+import { MyHomeMenuItems } from '../../helpers/launchpad/MenuItems.helper'
+
+export const MyHomes = () => {
+  return (
+    <Container variant="tertiary" minW="full" h="30rem">
+      <ContainerHeader
+        title={t('myHomes.title')}
+        titleIcon={House}
+        menuItems={MyHomeMenuItems}
+        icon={<CustomIcon type={FiMoreHorizontal} size="8" />}
+      />
+    </Container>
+  )
+}
+
+export default MyHomes
