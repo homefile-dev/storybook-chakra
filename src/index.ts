737b3de17c9239760e9156a5851846cd8153e965
Grasiela Gomes <produtoradama@gmail.com>
2022-04-04 20:37:08 -0300

    fix: export components

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
 export { SignUp } from './components/pages/SignUp';
+export { SignIn } from './components/pages/SignIn';
+export { ResetPassword } from './components/pages/ResetPassword';
+export { ActivateAccount } from './components/pages/ActivateAccount';
+export { EmailValidation } from './components/pages/EmailValidation';
+export { Launchpad } from './components/pages/Launchpad';
