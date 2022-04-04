0a1e14109c6ef343346fac8e1d6357fd1b1067d2
Grasiela Gomes <produtoradama@gmail.com>
2022-04-04 20:42:39 -0300

    feat: add ContainerHeader component

---
+++ src/components/launchpad/ContainerHeader.tsx
@@ -0,0 +1,18 @@
+import { Flex, Text, Image } from '@chakra-ui/react'
+import { IContainerHeader } from '../../interfaces/launchpad/ContainerHeader.interface'
+import IconMenu from './IconMenu'
+
+export const ContainerHeader = ({ icon, menuItems, title, titleIcon }: IContainerHeader) => (
+  <Flex justifyContent="space-between" bg="white" align="center" px="4" py="4">
+    <Flex gap="2">
+      <Image src={titleIcon} />
+      <Text>{title}</Text>
+    </Flex>
+    <IconMenu
+      icon={icon}
+      menuItems={menuItems}
+    />
+  </Flex>
+)
+
+export default ContainerHeader
---
+++ src/interfaces/launchpad/ContainerHeader.interface.ts
@@ -0,0 +1,6 @@
+import { IIconMenu } from "./IconMenu.interface"
+
+export interface IContainerHeader extends IIconMenu {
+  title: string
+  titleIcon: string
+}
