af63064bde476da79dc83523da89515fd46d0aa3
Grasiela Gomes <produtoradama@gmail.com>
2022-04-16 11:51:30 -0300

    fix: change env var name

--- src/apis/locationApi.ts
+++ src/apis/locationApi.ts
@@ -1,12 +1,12 @@
 import axios from 'axios'
 
 const locationApi = axios.create({
-  baseURL: process.env.LOCATION_BASE_URL,
+  baseURL: process.env.REACT_APP_LOCATION_BASE_URL,
   params: {
     limit: 1,
     language: 'en',
     country: 'us',
-    access_token: process.env.MAPBOX_TOKEN,
+    access_token: process.env.REACT_APP_MAPBOX_TOKEN,
   },
 })
 
